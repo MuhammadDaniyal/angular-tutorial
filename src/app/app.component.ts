85947bfb5080c6aafbef875c4bd5d9ccfcdddf53
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-08-20 23:41:55 +0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tut';
+  data = 'hello';
+  getValue() {
+    return 'Get Value Function';
+  }
 }
