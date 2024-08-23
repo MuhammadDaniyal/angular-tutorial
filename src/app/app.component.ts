99025b022039b7c18a51f8dae93efe6dbdcccfe3
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-08-24 03:45:55 +0500

    improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-tut';
   data = 'hello';
+  displayName = '';
   getValue() {
     return 'Get Value Function';
+  }
+  getName(name: string) {
+    alert('Get Name Function ' + name);
+  }
+  getData(val: string) {
+    console.log('get Data Function', val);
+  }
+  getNameValue(val: string) {
+    this.displayName = val;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
